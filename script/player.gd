f4e0e927a8270eaa416878a2fcff972f17cc7fa7
Siarhei Doulgerou <siarhei.doulgerou@gmail.com>
2023-12-13 20:30:24 +0300

    Added arts, created new player scene and the main world scene, processed animations for directional moving for the player and created moving player's script

---
+++ script/player.gd
@@ -0,0 +1,35 @@
+extends CharacterBody2D
+
+var speed = 100
+var player_state
+
+func _physics_process(delta):
+	var direction = Input.get_vector("left", "right", "up", "down")
+	player_state = "idle" if (direction == Vector2.ZERO) else "walking"
+	
+	velocity = direction * speed
+	move_and_slide()
+	
+	play_anim(direction)
+	
+func play_anim(direction):
+	if  (player_state == "idle"):
+		$AnimatedSprite2D.play("idle")
+	if (player_state == "walking"):
+		if direction.y == -1:
+			$AnimatedSprite2D.play("n-walk")
+		if direction.x == -1:
+			$AnimatedSprite2D.play("w-walk")
+		if direction.y == 1:
+			$AnimatedSprite2D.play("s-walk")
+		if direction.x == 1:
+			$AnimatedSprite2D.play("e-walk")
+		if direction.x > 0.5 and direction.y < -0.5:
+			$AnimatedSprite2D.play("ne-walk")
+		if direction.x > 0.5 and direction.y > 0.5:
+			$AnimatedSprite2D.play("se-walk")
+		if direction.x < -0.5 and direction.y > 0.5:
+			$AnimatedSprite2D.play("sw-walk")
+		if direction.x < -0.5 and direction.y < -0.5:
+			$AnimatedSprite2D.play("nw-walk")
+	
